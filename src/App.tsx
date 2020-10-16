305b4d7c0ea4533df60f47fa5562c489d49fca40
allexis096 <allexissoares@gmail.com>
2020-10-16 02:05:16 -0300

    :zap: chore: using navContext on App and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
+import { NavContextProvider } from './context/navContext';
 
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes />
+    <>
+      <BrowserRouter>
+        <NavContextProvider>
+          <Routes />
+        </NavContextProvider>
+      </BrowserRouter>
+
       <GlobalStyles />
-    </BrowserRouter>
+    </>
   );
 };
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Login from '../pages/Login';
+import Dashboard from '../pages/Dashboard';
+import Balance from '../pages/Balance';
+import Extract from '../pages/Extract';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Login} />
+    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/balance" component={Balance} />
+    <Route path="/extract" component={Extract} />
   </Switch>
 );
 
