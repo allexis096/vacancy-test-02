a791deac638215813f6d84b10e3be8afc2f74517
allexis096 <allexissoares@gmail.com>
2020-10-20 21:28:58 -0300

    :zap: chore: dashboard page stylized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,23 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import navContext from '../../context/navContext';
-
 import LeftBar from '../../components/LeftBar';
+import api from '../../services/api';
 
-import { Container, DashboardStart } from './styles';
+import { Container, DashboardStart, Company } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [company] = useState(() => {
+    const companyName = localStorage.getItem('@Contasimples:company');
+
+    if (companyName) {
+      return JSON.parse(companyName);
+    }
+
+    return '';
+  });
+
   const history = useHistory();
 
   const { home, setHome } = useContext(navContext);
@@ -21,7 +31,12 @@ const Dashboard: React.FC = () => {
     <Container>
       <LeftBar />
       <DashboardStart>
-        <h1>DASHBOARD</h1>
+        <h1>Bem vindo à conta, {company}!</h1>
+
+        <Company>
+          <p>EMPRESA UM LTDA ME, CNPJ: 13.182.905/0001-71</p>
+          <span>Banco: 999, CONTA SIMPLES, Ag 1, Conta 123456-1</span>
+        </Company>
       </DashboardStart>
     </Container>
   );
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -6,4 +6,43 @@ export const Container = styled.div`
   height: 100vh;
 `;
 
-export const DashboardStart = styled.section``;
+export const DashboardStart = styled.section`
+  color: #fff;
+  font-weight: 700;
+
+  h1 {
+    padding: 20px;
+    font-size: 30px;
+  }
+
+  background-color: #45b34a;
+  opacity: 0.9;
+  flex: 1;
+`;
+
+export const Company = styled.main`
+  padding: 20px;
+  margin-left: 20px;
+  line-height: 40px;
+  width: 360px;
+
+  border: 1px solid #fff;
+  border-radius: 5px;
+  box-shadow: 2px 1px 3px 1px rgba(0, 0, 0, 0.67);
+
+  p {
+    font-size: 24px;
+  }
+
+  span {
+    display: block;
+    margin-top: 15px;
+  }
+
+  transition: margin-left 0.4s;
+  &:hover {
+    margin-left: 25px;
+    box-shadow: 5px 3px 3px 1px rgba(0, 0, 0, 0.67);
+    cursor: default;
+  }
+`;
