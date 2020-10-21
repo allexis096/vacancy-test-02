bac4284a7fe76ff6484609c57f1167f5c913ab98
allexis096 <allexissoares@gmail.com>
2020-10-21 11:33:26 -0300

    :recycle: refactor: substuition to common export

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({
+const apiTransactions = axios.create({
   baseURL: 'http://localhost:5000/',
 });
 
-export default api;
+export { apiTransactions };
