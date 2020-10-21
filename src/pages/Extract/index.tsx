7980f72ab7147da6ced7e3095e52338a40b6bce7
allexis096 <allexissoares@gmail.com>
2020-10-21 11:33:46 -0300

    :construction: chore: adjusting extract page

--- src/pages/Extract/index.tsx
+++ src/pages/Extract/index.tsx
@@ -5,23 +5,58 @@ import navContext from '../../context/navContext';
 
 import LeftBar from '../../components/LeftBar';
 
-import { Container, DashboardExtract } from './styles';
+import { Container, DashboardExtract, MainTable } from './styles';
 
 const Extract: React.FC = () => {
   const history = useHistory();
 
-  const { extract, setExtract } = useContext(navContext);
+  const { extract, setExtract, transactions } = useContext(navContext);
 
   useEffect(() => {
     setExtract(true);
     history.push('/extract');
+
+    const companyName = localStorage.getItem('@Contasimples:company');
+
+    if (!companyName) {
+      history.push('/');
+    }
   }, [extract, setExtract, history]);
 
   return (
     <Container>
       <LeftBar />
       <DashboardExtract>
-        <h1>EXTRATO</h1>
+        <h1>EXTRATO DETALHADO DA CONTA</h1>
+
+        <MainTable>
+          <table>
+            <thead>
+              <tr>
+                <th>Título</th>
+                <th>Preço</th>
+                <th>Categoria</th>
+                <th>Data</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {transactions.map(transaction => {
+                return (
+                  <tr key={transaction.formattedValue}>
+                    <td>{transaction.tipoTransacao}</td>
+                    <td className={transaction.formattedCredit}>
+                      {transaction.credito === true ? '+' : '-'}{' '}
+                      {transaction.formattedValue}
+                    </td>
+                    <td>{transaction.descricaoTransacao}</td>
+                    <td>{transaction.formattedDate}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </MainTable>
       </DashboardExtract>
     </Container>
   );
--- src/pages/Extract/styles.ts
+++ src/pages/Extract/styles.ts
@@ -3,7 +3,62 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   width: 100vw;
-  height: 100vh;
+  /* height: 100vh; */
 `;
 
-export const DashboardExtract = styled.section``;
+export const DashboardExtract = styled.section`
+  color: #fff;
+  font-weight: 700;
+
+  h1 {
+    padding: 20px;
+    font-size: 30px;
+    text-align: center;
+  }
+
+  background-color: #45b34a;
+  opacity: 0.9;
+  flex: 1;
+`;
+
+export const MainTable = styled.main`
+  color: #000;
+  background-color: #e5ebe7;
+  width: max-content;
+  margin: 0 auto 20px;
+
+  border: 1px solid #fff;
+  border-radius: 5px;
+  box-shadow: 2px 1px 3px 1px rgba(0, 0, 0, 0.67);
+
+  table {
+    width: 100%;
+    border-spacing: 0 8px;
+
+    th {
+      color: #000;
+      font-weight: normal;
+      padding: 20px 32px;
+      text-align: left;
+      font-size: 16px;
+      line-height: 10px;
+    }
+
+    td {
+      padding: 20px 32px;
+      border: 0;
+      background: #fff;
+      font-size: 16px;
+      font-weight: normal;
+      color: #000;
+
+      &.credit {
+        color: #45b34a;
+      }
+
+      &.debit {
+        color: #ff0000;
+      }
+    }
+  }
+`;
